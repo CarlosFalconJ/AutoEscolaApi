eda9d7ec4ef88e41f61f0f24cc9c45da426194bc
Carlos <carlos.falcon@ftdata.com.br>
2023-10-25 11:04:37 -0300

    feat: Create entity vehicle

---
+++ src/Entity/Vehicle.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\VehicleRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: VehicleRepository::class)]
+class Vehicle
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $plate = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $model = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $color = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $renavam = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getPlate(): ?string
+    {
+        return $this->plate;
+    }
+
+    public function setPlate(string $plate): static
+    {
+        $this->plate = $plate;
+
+        return $this;
+    }
+
+    public function getModel(): ?string
+    {
+        return $this->model;
+    }
+
+    public function setModel(string $model): static
+    {
+        $this->model = $model;
+
+        return $this;
+    }
+
+    public function getColor(): ?string
+    {
+        return $this->color;
+    }
+
+    public function setColor(string $color): static
+    {
+        $this->color = $color;
+
+        return $this;
+    }
+
+    public function getRenavam(): ?string
+    {
+        return $this->renavam;
+    }
+
+    public function setRenavam(string $renavam): static
+    {
+        $this->renavam = $renavam;
+
+        return $this;
+    }
+}
